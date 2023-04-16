329e49880fd22e0ef786876395af3510680b488e
shiromofufactory <shiromofufactory@gmail.com>
2023-04-16 12:04:55 +0900

    play.py 再生と停止機能

--- play.py
+++ play.py
@@ -9,18 +9,23 @@ class App:
         pyxel.init(160, 120, title="Pyxel Tracker Player")
         with open(f"./musics/{MUSIC_FILE}.json", "rt") as fin:
             self.music = json.loads(fin.read())
-        for ch, sound in enumerate(self.music):
-            pyxel.sound(ch).set(*sound)
-            pyxel.play(ch, ch, loop=True)
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            if pyxel.play_pos(0) is None:
+                for ch, sound in enumerate(self.music):
+                    pyxel.sound(ch).set(*sound)
+                    pyxel.play(ch, ch, loop=True)
+            else:
+                pyxel.stop()
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(40, 57, "Press [ESC] to exit.", 7)
+        pyxel.text(20, 54, "Press [SPACE] to play / stop.", 7)
+        pyxel.text(20, 60, "Press [ESC] to exit.", 7)
 
 
 App()
